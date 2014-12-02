ad57bb1c4e859f9ec56799b220349ede66fc732f
Martok <martok@martoks-place.de>
2014-12-02 02:42:22 +0100

    OpenSSL SSL API Plugin

---
+++ src/core/sslapi_openssl/version.h
@@ -0,0 +1,14 @@
+
+#include <m_version.h>
+
+#define __FILEVERSION_STRING      MIRANDA_VERSION_FILEVERSION
+#define __VERSION_STRING         MIRANDA_VERSION_STRING
+
+#define __PLUGIN_NAME            "OpenSSL SSL API module"
+#define __INTERNAL_NAME            "sslapi_openssl"
+#define __FILENAME               "sslapi_openssl.dll"
+#define __DESCRIPTION             "Core module to provide OpenSSL services."
+#define __AUTHOR                 "Miranda NG Development Team"
+#define __AUTHOREMAIL            ""
+#define __AUTHORWEB               "http://miranda-ng.org/p/StdCrypt/"
+#define __COPYRIGHT               "© 2014 Miranda NG Development Team"
